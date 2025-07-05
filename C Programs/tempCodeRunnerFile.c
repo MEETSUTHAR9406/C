8c5d4ee38e05ddcdf348d885aae33faa69ef6a70
MEETSUTHAR9406 <meetsuthar9406@gmail.com>
2025-07-06 01:33:11 +0530

    Changed location of file.

---
+++ C Programs/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+    // for(int i = 0; i < row1; i++) {
+    //     for(int j = 0; j < column1; j++) {
+    //         printf("At index A[%d][%d], The value is %d", i, j, A[i][j]);
+    //     }
+    // }

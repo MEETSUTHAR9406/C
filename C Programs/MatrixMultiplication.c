663600597443174b5076e7f164eb84cdcd3b6d54
MEETSUTHAR9406 <meetsuthar9406@gmail.com>
2025-07-06 14:34:26 +0530

    Complete Program of Matrix Multiplication

--- C Programs/MatrixMultiplication.c
+++ C Programs/MatrixMultiplication.c
@@ -1,60 +1,72 @@
 #include <stdio.h>
 
-void MatrixA(int row1, int col1) {
+void MatrixA(int row1, int col1, int A[row1][col1]) {
 
-    int A[row1][col1];
     // Taking input for Matrix A
     printf("Enter values of Matrix A:\n");
     for (int i = 0; i < row1; i++) {
         for (int j = 0; j < col1; j++) {
             printf("Element [%d][%d]: ", i, j);
-            scanf("%d", &A[i][j]);
+            if (scanf("%d", &A[i][j]) != 1) {
+                printf("Invalid input.\n");
+                return;
+            }
         }
         printf("\n");
     }
 
+    // Printing the Matrix A
     printf("\nMatrix A is:\n");
     for (int i = 0; i < row1; i++) {
         for (int j = 0; j < col1; j++) {
-            printf("%d ", A[i][j]);
+            printf("%4d ", A[i][j]);
         }
         printf("\n");
     }
 }
 
-void MatrixB(int row2, int col2) {
+void MatrixB(int row2, int col2, int B[row2][col2]) {
 
-    int B[row2][col2];
 
     // Taking input for Matrix B
     printf("\nEnter values of Matrix B:\n");
     for (int i = 0; i < row2; i++) {
         for (int j = 0; j < col2; j++) {
             printf("Element [%d][%d]: ", i, j);
-            scanf("%d", &B[i][j]);
+            if (scanf("%d", &B[i][j]) != 1) {
+                printf("Invalid input.\n");
+                return;
+            }
         }
     }
 
+    // Printing the Matrix B
     printf("\nMatrix B is:\n");
     for (int i = 0; i < row2; i++) {
         for (int j = 0; j < col2; j++) {
-            printf("%d ", B[i][j]);
+            printf("%4d ", B[i][j]);
         }
         printf("\n");
     }
 }
 
-void MatrixC(int row1, int row2, int col1, int col2, int MatrixA[row1][col1], int MatrixB[row2][col2]) {
+void MatrixC(int row1, int row2, int col1, int col2, int A[row1][col1], int B[row2][col2]) {
 
-    int sum;
-    int C[row1][col1];
+    int C[row1][col2];
 
-    for(int i = 0; i < col1; i++) {
-        for(int j = 0; j < row2; j++) {
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < col2; j++) {
             C[i][j] = 0;
             for(int k = 0; k < col1; k++) {
-                sum = MatrixA[i][j] + MatrixB[j][i];
+                C[i][j] += A[i][k] * B[k][j];
             }
+        }
+    }
+
+    printf("\nMultiplication Matrix is:\n");
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < col2; j++) {
+            printf("%4d ", C[i][j]);
         }
         printf("\n");
     }
@@ -65,12 +77,16 @@ int main() {
     int row1, row2, col1, col2;
 
     printf("Enter number of rows and col for Matrix A: ");
-    scanf("%d %d", &row1, &col1);
+    if (scanf("%d %d", &row1, &col1) != 2) {
+        printf("Invalid input.\n");
+        return 1;
+    }
 
     printf("Enter number of rows and col for Matrix B: ");
-    scanf("%d %d", &row2, &col2);
-
-    // printf("You want to do Matrix Multiplication of Matrix A(%d X %d) and Matrix B(%d X %d).", row1, col1, row2, col2);
+    if (scanf("%d %d", &row2, &col2) != 2) {
+        printf("Invalid input.\n");
+        return 1;
+    }
 
     if (col1 == row2) {
         printf("Matrix Multiplication is possible for Matrix A(%d X %d) and Matrix B(%d X %d).\n", row1, col1, row2, col2);
@@ -79,21 +95,12 @@ int main() {
         return 1;
     }
 
-    MatrixA(row1, col1);
-    MatrixB(row2, col2);
-    MatrixC(row1, row2, col1, col2, MatrixA, MatrixB);
+    int A[row1][col1], B[row2][col2];
 
-    // int sum;
-    // int C[row1][col1];
+    MatrixA(row1, col1, A);
+    MatrixB(row2, col2, B);
 
-    // for(int i = 0; i < col1; i++) {
-    //     for(int j = 0; j < row2; j++) {
-    //         C[i][j] = 0;
-    //         for(int k = 0; k < row2; k++) {
-    //             sum = MatrixA[i][j] + MatrixB[j][i];
-    //         }
-    //     }
-    //     printf("\n");
-    // }
+    MatrixC(row1, row2, col1, col2, A, B);
+
     return 0;
 }

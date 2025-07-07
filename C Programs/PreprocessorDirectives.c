eecd5508bc8600edd449ae090b51acd80a23ce8a
MEETSUTHAR9406 <meetsuthar9406@gmail.com>
2025-07-07 14:38:20 +0530

    Added macros and system predefined macros demo with Palindrome include

---
+++ C Programs/PreprocessorDirectives.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "Palindrome.c"
+#define PI 3.14
+#define SQUARE(x) x * x
+// #undef PI
+
+int main() {
+
+    float var = PI;
+    int area = 5;
+    printf("This is PI %.2f\n", var);
+    printf("The area is %f", PI * SQUARE(area));
+
+
+    printf("\nToday Date is %s", __DATE__); // Fetches current date from the system.
+    printf("\nANSI: %d", __STDC__);
+    printf("\nFile name is %s", __FILE__); // Fetches the File name from the system. 
+    printf("\nLine number is %d", __LINE__); // Fetches the Line number where this is written from the system.
+    printf("\nCurrent time is %s", __TIME__); // Fetches the current time from the system.
+
+    return 0;
+}

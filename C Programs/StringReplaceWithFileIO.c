15f83c70d062bfa9aa029bc4049462982e09e623
MEETSUTHAR9406 <MEETSUTHAR9406@users.noreply.github.com>
2025-07-08 19:14:03 +0530

    Implement string replacement for template-based bills

--- C Programs/StringReplaceWithFileIO.c
+++ C Programs/StringReplaceWithFileIO.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *replaceWord(const char *str, const char *oldWord, const char *newWord) {
     char *resultString;
@@ -9,21 +9,21 @@ char *replaceWord(const char *str, const char *oldWord, const char *newWord) {
     int oldWordLength = strlen(oldWord);
 
     for(i = 0; str[i] != '\0'; i++) {
-        if (strstr(&str[i], oldWord) == &str[i]) {
+        if(strstr(&str[i], oldWord) == &str[i]) {
             count++;
             i = i + oldWordLength - 1;
         }
     }
 
     resultString = (char *) malloc(i + count * (newWordLength - oldWordLength) + 1);
-    if (resultString == NULL) {
+    if(resultString == NULL) {
         printf("Memory allocation failed.\n");
         exit(1);
     }
 
     i = 0;
-    while (*str) {
-        if (strstr(str, oldWord) == str) {
+    while(*str) {
+        if(strstr(str, oldWord) == str) {
             strcpy(&resultString[i], newWord);
             i += newWordLength;
             str += oldWordLength;
@@ -37,17 +37,19 @@ char *replaceWord(const char *str, const char *oldWord, const char *newWord) {
 }
 
 int main() {
+
     FILE *file_ptr = fopen("Bill.txt", "r");
     FILE *file_ptr2 = fopen("NewBill.txt", "w");
 
-    if (file_ptr == NULL || file_ptr2 == NULL) {
+    if(file_ptr == NULL || file_ptr2 == NULL) {
         printf("File not found.\n");
         return 1;
     }
 
     char str[1000];
 
-    while (fgets(str, sizeof(str), file_ptr) != NULL) {
+    while(fgets(str, sizeof(str), file_ptr) != NULL) {
+
         char *newStr = replaceWord(str, "{{name}}", "Meet");
 
         char *temp = newStr;
@@ -58,10 +60,10 @@ int main() {
         newStr = replaceWord(newStr, "{{outlet}}", "Samsung");
         free(temp);
 
-        printf("%s", newStr);                    
-        fprintf(file_ptr2, "%s", newStr);         
+        printf("%s", newStr);
+        fprintf(file_ptr2, "%s", newStr);
 
-        free(newStr);                             
+        free(newStr);
     }
 
     fclose(file_ptr);
